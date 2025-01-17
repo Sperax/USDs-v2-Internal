23c044c50d82e74dc56b79a00033f610a83c6def
Dayaa <m.d.e.boutalbi@me.com>
2025-01-17 12:45:50 +0100

    Update ILPRewarder interface to return multiple reward tokens and their amounts

--- contracts/strategies/stargateV2/interfaces/ILPRewarder.sol
+++ contracts/strategies/stargateV2/interfaces/ILPRewarder.sol
@@ -1,5 +1,6 @@
 pragma solidity 0.8.19;
 
 interface ILPRewarder {
-    function getRewards(address lpToken, address user) external view returns (uint256);
+    function getRewards(address lpToken, address user) external view returns (address[] memory, uint256[] memory);
+    function rewardTokens() external view returns (address[] memory);
 }
